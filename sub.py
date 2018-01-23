d248580e6d76d1d733eff8551724e71b26be1546
Anulekha06 <35715671+Anulekha06@users.noreply.github.com>
2018-01-23 13:23:31 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,13 @@
+#program subtraction, multiplication, module,division
+a=input("enter the value of a")
+b=input("enter the value of b")
+c=int(a)-int(b)
+print(c)
+d=int(a)*int(b)
+print(d)
+e=int(a)%int(b)
+print(e)
+f=int(a)/int(b)
+print(f)
+
+
