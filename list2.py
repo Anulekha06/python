b20bebdff6106f7b55b082139a34185eca5a90e6
Anulekha06 <35715671+Anulekha06@users.noreply.github.com>
2018-02-06 10:24:50 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,17 @@
+#list2
+a=[12,3,4,6,7,89,9]
+b=["ani","anu","ilu"]
+c=["names",b,a]
+print(c)
+
+a=[12,3,4,6,7,89,9]
+b=["ani","anu","ilu"]
+c=["names",b,a]
+c[0]="lallaa"
+print(c)
+
+a=[12,3,4,6,7,89,9]
+b=["ani","anu","ilu"]
+c=["names",b,a]
+d=a+b+c
+print(d)
