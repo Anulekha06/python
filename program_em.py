0a96a7d6ece6a25938e9bde52daf46576f4a1bde
Anulekha06 <35715671+Anulekha06@users.noreply.github.com>
2018-02-20 10:36:50 +0530

    Add files via upload

---
+++ program_em.py
@@ -0,0 +1,9 @@
+#email
+import smtplib
+s=smtplib.SMTP("smtp.gmail.com",587)
+s.starttls()
+s.login("anulekha2112016@gmail.com","124052anu664")
+msg="hi, how are you? "
+s.sendmail("anulekha2112016@gmail.com","yashaswithakur1234@gmail.com",msg)
+print("success")
+s.quit()
