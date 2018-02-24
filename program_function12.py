1d02d5cd59556b14adf10b4d9af03977dfb7602b
Anulekha06 <35715671+Anulekha06@users.noreply.github.com>
2018-02-24 10:07:50 +0530

    Add files via upload

---
+++ program_function12.py
@@ -0,0 +1,10 @@
+#program for demonstrating function
+def add(a,b):
+    #print(a+b)
+    return a+b
+def div(a,b):
+    return a/b
+print("my answer is 5")
+x=4
+y=6
+print("addition of x,y",add(x,y),"div of x,y",div(x,y))
