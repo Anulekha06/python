7ea55fdfb24169fb29980b5f62de29ceaeb7fe2f
Anulekha06 <35715671+Anulekha06@users.noreply.github.com>
2018-02-27 08:29:56 +0530

    Update program_em.py

--- program_em.py
+++ program_em.py
@@ -2,7 +2,7 @@
 import smtplib
 s=smtplib.SMTP("smtp.gmail.com",587)
 s.starttls()
-s.login("anulekha2112016@gmail.com","124052anu664")
+s.login("anulekha2112016@gmail.com","********")
 msg="hi, how are you? "
 s.sendmail("anulekha2112016@gmail.com","yashaswithakur1234@gmail.com",msg)
 print("success")
